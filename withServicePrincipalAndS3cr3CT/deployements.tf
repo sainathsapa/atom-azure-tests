cf60ee76d0881bb77f6e777bcf53fe9687bbbc7e
Sainath Sapa <info.djsai@gmail.com>
2022-09-12 11:16:12 +0530

    Connection with Kube

--- withServicePrincipalAndS3cr3CT/deployements.tf
+++ withServicePrincipalAndS3cr3CT/deployements.tf
@@ -1,3 +1,16 @@
+/* Connection with Kube */
+data "azurerm_kubernetes_cluster" "forconnection" {
+  name                = "AtomKubeCluster"
+  resource_group_name = var.resource_group_name
+}
+
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.forconnection.kube_config.0.host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.forconnection.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.forconnection.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.forconnection.kube_config.0.cluster_ca_certificate)
+}
+
 /* Deploy the Fluentd */
 resource "kubernetes_deployment" "fluentd" {
   metadata {
